c6c084fae51a2a13b4729f3e9c947a2cd8b21a5a
johniblake <johniblake93@gmail.com>
2019-06-25 11:59:42 -0500

    added dummy api and seed routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,26 @@
+const router = require("express").Router();
+const db = require("../models");
+const seeds = require("../charSeeds.json");
+
+router.route("/characters").get((req, res, err) => {
+  res.json(seeds);
+});
+router.route("/character").post((req, res, err) => {
+  res.send("");
+});
+router
+  .route("/character/:id")
+  .get((req, res, err) => {
+    res.json(seeds[0]);
+  })
+  .put((req, res, err) => {
+    res.json("");
+  })
+  .post((req, res, err) => {
+    res.json("");
+  });
+router.route("/characters/mine").get((req, res, err) => {
+  res.json(seeds);
+});
+
+module.exports = router;
